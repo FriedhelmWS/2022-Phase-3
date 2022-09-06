d2c0b1ab732e2c03d1cac55c8606096db97e201a
FriedhelmWS <352063465@qq.com>
2022-09-07 00:20:41 +1200

    Edit cors v5

--- back-end-assignment/Assignment/Startup.cs
+++ back-end-assignment/Assignment/Startup.cs
@@ -19,7 +19,7 @@ namespace Assignment
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddCors();
+            services.AddCors(c => { c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin()); });
             services.AddControllers();
             
 
