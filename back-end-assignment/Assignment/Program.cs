6d7641423d4c3ac458c4910ecceefc871716b576
FriedhelmWS <352063465@qq.com>
2022-09-07 03:35:13 +1200

    Enable swagger

--- back-end-assignment/Assignment/Program.cs
+++ back-end-assignment/Assignment/Program.cs
@@ -26,11 +26,8 @@ app.UseCors(builder =>
 });
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
